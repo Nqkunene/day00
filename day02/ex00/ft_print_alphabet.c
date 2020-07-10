465a5dcc50e771c70d5628acf230392f0b7e3a4c
Nqobile Kunene <nqkunene@c4r6s8.wethinkcode.co.za>
2020-07-10 11:07:42 +0200

    this ex00

---
+++ day02/ex00/ft_print_alphabet.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+int ft_putchar(char c)
+{ 
+	write(1, &c, 1);
+	 return (0); 
+}
+
+void ft_print_alphabet(void)
+{
+   	char letter = 'a';
+		 while(letter <= 'z') 
+		{ 
+			ft_putchar(letter);
+		   	letter++;
+	   	}
+}
+
+
